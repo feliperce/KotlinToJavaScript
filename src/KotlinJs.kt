704e1449887beede888b593d48fa990993627e27
Felipe Rodrigues <felipe.rce@gmail.com>
2018-07-01 02:04:12 -0300

    Added number format check

--- src/KotlinJs.kt
+++ src/KotlinJs.kt
@@ -1,14 +1,28 @@
 import kotlin.browser.*
+import org.w3c.dom.*
 
 fun main(args: Array<String>) {
 
-    val button = document.getElementById("submit")
+    val button = document.getElementById("submit") as HTMLInputElement
+    val text = document.getElementById("note") as HTMLInputElement
 
-    button?.addEventListener("click", {
-        window.alert("Um alert, eita!!!")
+    button.addEventListener("click", {
+        window.alert(evaluate(text.value))
     })
 }
 
-fun valuate() {
+fun evaluate(value: String): String {
 
+    val number: Int? = value.toIntOrNull()
+
+    return if(number != null) {
+        when (number) {
+            in 0..3 -> "Que absurdo!"
+            in 4..6 -> "Invejoso!!!"
+            in 7..10 -> "s2 Kotlin é amor s2"
+            else -> "Informe somente número de 0 a 10, der."
+        }
+    } else {
+        "Informe apenas números!!"
+    }
 }
